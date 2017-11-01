30ac32848c778b9479542847ec734c94c2b4fdc0
KiKi Chen <32251561+jacchkiki@users.noreply.github.com>
2017-11-01 21:52:44 +0800

    1101

---
+++ 1101.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+print "陳顗丞"
+
+
+for sg in range(0,10):
+    if sg==7:
+        print "lucky"
+    print sg
+
+tr=open("1.txt","w+")
+tr.write("fg")
+tr.close()
+
+tr=open("name.txt","w+")
+tr.write("kikipi")
+tr.close()
+
+tr=open("2.txt","w+")
+tr.write("2")
+tr.close()
+
+
+tr=open("3.txt","w+")
+tr.write("3")
+tr.close()
+
+tr=open("4.txt","w+")
+tr.write("4")
+tr.close()
